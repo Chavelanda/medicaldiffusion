67c409a66bf71d10f1ea717b48a59d156d9db79e
Giacomo Melacini <gmelacini@unibz.it>
2023-11-23 15:10:51 +0100

    Added online semi-hard negative mining

--- filtering/retrieval.py
+++ filtering/retrieval.py
@@ -2,11 +2,13 @@ from typing import Any
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 import medicalnet as mednet
 
 class RetrievalResnet(pl.LightningModule):
-    def __init__(self, cfg) -> None:
+    def __init__(self, cfg, verbose=False) -> None:
         super().__init__()
         self.cfg = cfg
 
@@ -22,16 +24,71 @@ class RetrievalResnet(pl.LightningModule):
 
         self.resnet = mednet.resnet_gap(cfg.model.resnet_type, pretrain_path=
                                         cfg.model.pretrain_path, **model_params)
-        self.criterion = nn.TripletMarginLoss()
+        self.margin = 1.0
 
+        self.verbose = verbose
 
+    def triplet_margin_loss_online(self, x):
+        B, I, C = x.shape
+        element_range = torch.arange(I)
+        batch_range = torch.arange(B)
+
+        # Get all combinations of positive_range for anchor and positive examples
+        element_combinations = torch.combinations(element_range, r=2)
+        
+        # Initialize a list to store the triplets_indices
+        triplets_indices = []
+
+        # Iterate over the batch size for positive examples
+        for b_positive in batch_range:
+            # Iterate over the combinations
+            for element1, element2 in element_combinations:  
+                # Iterate over the batch size for negative examples
+                for b_negative in batch_range:
+                    # Ensure the negative example is from a different batch
+                    if b_negative != b_positive:
+                        # Iterate over the negative indices
+                        for negative in element_range:
+                            # Add the triplet to the list
+                            triplets_indices.append(((b_positive, element1), (b_positive, element2), (b_negative, negative)))
+
+        # Convert the list of triplets_indices to a tensor
+        triplets_indices = torch.tensor(triplets_indices) 
+        triplets_indices = triplets_indices.view(-1, 2) 
+        
+        # Index x with triplets_indices
+        triplets = x[triplets_indices[:, 0], triplets_indices[:, 1]]
+
+        # Reshape triplets for triplet loss calculation
+        triplets = triplets.view(-1, 3, C)
+        
+        # Mine hard negatives
+        positive_anchor_distances = F.pairwise_distance(triplets[:, 1], triplets[:, 0]) # torch.cdist(triplets[:, 1].unsqueeze(1), triplets[:, 0].unsqueeze(1)).squeeze(1) # torch.norm(triplets[:, 0] - triplets[:, 1], dim=1)
+        negative_anchor_distances = F.pairwise_distance(triplets[:, 2], triplets[:, 0]) # torch.cdist(triplets[:, 2].unsqueeze(1), triplets[:, 0].unsqueeze(1)).squeeze(1) # torch.norm(triplets[:, 0] - triplets[:, 2], dim=1)
+        positive_anchor_margin_distances = positive_anchor_distances + self.margin
+
+        semi_hard_negative_mask = (positive_anchor_distances < negative_anchor_distances) & (negative_anchor_distances < positive_anchor_margin_distances)
+        
+        masked_positive_anchor_distances = positive_anchor_distances[semi_hard_negative_mask]
+        masked_negative_anchor_distances = negative_anchor_distances[semi_hard_negative_mask]
+
+        # Calculate triplet margin loss
+        loss = torch.clamp_min(self.margin + masked_positive_anchor_distances - masked_negative_anchor_distances, 0)
+        return torch.mean(loss)
+    
     def forward(self, batch) -> Any:
-        x1, x2, x3 = batch['anchor'], batch['positive'], batch['negative']
+        x = batch['data']
 
-        x1, x2, x3 = self.resnet(x1), self.resnet(x2), self.resnet(x3)
+        B, I, C, D, H, W = x.shape
+        
+        x_batched = x.view(B*I, C, D, H, W)
+        
+        x_batched = self.resnet(x_batched)
 
-        loss = self.criterion(x1, x2, x3)
+        x = x_batched.view(B, I, -1)
 
+        loss = self.triplet_margin_loss_online(x)
+    
         return loss
 
 

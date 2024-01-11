a1410a3a58d7ed6e01c842fa7ab4641d18a6aacd
Giacomo Melacini <gmelacini@unibz.it>
2024-01-11 17:02:17 +0100

    cfg and scripts to compute ms-ssim

---
+++ dataset/allcts_msssim.py
@@ -0,0 +1,27 @@
+import numpy as np
+from allcts import AllCTsDataset
+
+
+class AllCts_MSSSIM(AllCTsDataset):
+
+    def __init__(self, root_dir, split, resize_d=1, resize_h=1, resize_w=1, samples=1000):
+        super().__init__(root_dir=root_dir, split=split, resize_d=resize_d, resize_h=resize_h, resize_w=resize_w)
+
+        self.samples = samples
+
+    def __len__(self):
+        return self.samples
+
+    def __getitem__(self, index):
+        index1 = np.random.randint(len(self.df))
+        index2 = np.random.randint(len(self.df))
+
+        img1 = super().__getitem__(index1)['data']
+        img2 = super().__getitem__(index2)['data']
+
+        return img1, img2
+    
+
+if __name__ == "__main__":
+    dataset = AllCts_MSSSIM(root_dir='data/AllCTs_nrrd_global', split='train', resize_d=1, resize_h=1, resize_w=1)
+    print(dataset[0][0].shape)
---
+++ evaluation/calculate_msssim.py
@@ -0,0 +1,50 @@
+import os
+import hydra
+from omegaconf import DictConfig, OmegaConf, open_dict
+
+from tqdm import tqdm
+import wandb
+import torch
+
+from pytorch_ssim import MSSSIM_3d
+from dataset.allcts_msssim import AllCts_MSSSIM
+
+
+@hydra.main(config_path='../config', config_name='base_cfg', version_base=None)
+def run(cfg: DictConfig):
+    if cfg.model.gpus != -1:
+        torch.cuda.set_device(cfg.model.gpus)
+    
+    with open_dict(cfg):
+        cfg.model.results_folder = os.path.join(
+            cfg.model.results_folder, cfg.model.run_name)
+
+    if not os.path.exists(cfg.model.results_folder):
+        os.makedirs(cfg.model.results_folder)
+
+    wandb.init(project=cfg.model.wandb_project, entity=cfg.model.wandb_entity, name=cfg.model.run_name)
+
+    wandb.config.update(OmegaConf.to_container(cfg.dataset))
+    wandb.config.update(OmegaConf.to_container(cfg.model))
+
+    dataset = AllCts_MSSSIM(root_dir=cfg.dataset.root_dir, split=cfg.dataset.split, samples=cfg.dataset.samples, resize_d=cfg.dataset.resize_d, resize_h=cfg.dataset.resize_h, resize_w=cfg.dataset.resize_w)
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg.model.batch_size, shuffle=True, num_workers=cfg.model.num_workers, pin_memory=True)
+
+    if cfg.dataset.samples % cfg.model.batch_size != 0:
+        raise ValueError('The number of samples must be divisible by the batch size.')
+
+    model = MSSSIM_3d(window_size=cfg.model.window_size, size_average=cfg.model.size_average, channel=cfg.dataset.image_channels).to(cfg.model.device)
+ 
+    sum = 0.0
+
+    with torch.no_grad():
+        for i, batch in tqdm(enumerate(dataloader)):
+            img1, img2 = batch.to(cfg.model.device)
+            msssim = model(img1, img2)
+            sum += msssim.item()
+            wandb.log({'msssim': sum/(i+1), 'samples': (i+1)*cfg.model.batch_size})
+
+
+if __name__ == "__main__":
+    run()

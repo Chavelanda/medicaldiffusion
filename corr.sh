cd6ed08f305b2cf27b803f5e9b8aeb20420c2ef4
Giacomo Melacini <giachimela@gmail.com>
2024-01-31 22:52:21 +0100

    added correlation

---
+++ corr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set CUDA environment variables
+export PATH="/usr/local/cuda-11.1/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-11.1/lib64:$LD_LIBRARY_PATH"
+export CUDA_VISIBLE_DEVICES=1
+export CPATH="/usr/local/cuda-11.1/include:$CPATH"
+
+# Activate Python virtual environment
+source .venv/bin/activate
+
+# Run Python script
+python -m temporary

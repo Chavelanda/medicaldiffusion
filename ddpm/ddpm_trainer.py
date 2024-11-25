Updated actual lr calculation, added warmup, fixed dataloader in ddp, changed conv to treat all dims equally

ddpm/ddpm_trainer.py
@@ -22,10 +22,11 @@ class Trainer(object):
     def __init__(
         self,
         diffusion_model,
-        dataset=None,
-        val_dataset=None,
+        dl=None,
+        val_dl=None,
         ema_decay=0.995,
         train_batch_size=32,
+        base_lr=1e-4,
         train_lr=1e-4,
         train_num_steps=100000,
         gradient_accumulate_every=2,
@@ -53,21 +54,17 @@ class Trainer(object):
         self.gradient_accumulate_every = gradient_accumulate_every
         self.train_num_steps = train_num_steps
         
-        self.ds = dataset
-        self.val_dataset = val_dataset
+        self.ds = dl.dataset
         
-        dl = DataLoader(self.ds, batch_size=train_batch_size,
-                        shuffle=True, pin_memory=True, num_workers=num_workers)
-        self.val_dl = DataLoader(self.val_dataset, batch_size=train_batch_size,
-                                  shuffle=False, pin_memory=True, num_workers=num_workers)
-
         self.len_dataloader = len(dl)
         self.dl = cycle(dl)
 
         assert len(self.ds) > 0, 'need to have at least 1 video to start training (although 1 is not great, try 100k)'
 
         self.opt = Adam(diffusion_model.parameters(), lr=train_lr)
-
+        start_factor = base_lr/train_lr
+        self.scheduler = torch.optim.lr_scheduler.LinearLR(self.opt, start_factor=start_factor, total_iters=5)
+        
         self.step = 0
 
         self.amp = amp
@@ -227,12 +224,16 @@ class Trainer(object):
                 val_log = self.validation_step(prob_focus_present, focus_present_mask)
                 log = {**log, **val_log}
 
-            log = {**log, 'global_step': self.step}
+            log = {**log, 'global_step': self.step, 'lr': self.scheduler.get_last_lr()}
 
             # Log
             log_fn(log)
             
             self.step += 1
+
+            # Update scheduler if epoch is over
+            if self.step % self.len_dataloader == 0:
+                self.scheduler.step()
 
             if self.rank == 0 and self.step % 100 == 0:
                 pbar.update(100)

ddpm/unet3d.py
@@ -54,8 +54,7 @@ class RelativePositionBias(nn.Module):
         q_pos = torch.arange(n, dtype=torch.long, device=device)
         k_pos = torch.arange(n, dtype=torch.long, device=device)
         rel_pos = rearrange(k_pos, 'j -> 1 j') - rearrange(q_pos, 'i -> i 1')
-        rp_bucket = self._relative_position_bucket(
-            rel_pos, num_buckets=self.num_buckets, max_distance=self.max_distance)
+        rp_bucket = self._relative_position_bucket(rel_pos, num_buckets=self.num_buckets, max_distance=self.max_distance)
         values = self.relative_attention_bias(rp_bucket)
         return rearrange(values, 'i j h -> h i j')
 
@@ -83,13 +82,13 @@ class SinusoidalPosEmb(nn.Module):
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
 
-
+# lappala
 def Upsample(dim):
-    return nn.ConvTranspose3d(dim, dim, (1, 4, 4), (1, 2, 2), (0, 1, 1))
+    return nn.ConvTranspose3d(dim, dim, (4, 4, 4), (2, 2, 2), (1, 1, 1))
 
-
+# lappala
 def Downsample(dim):
-    return nn.Conv3d(dim, dim, (1, 4, 4), (1, 2, 2), (0, 1, 1))
+    return nn.Conv3d(dim, dim, (4, 4, 4), (2, 2, 2), (1, 1, 1))
 
 
 class LayerNorm(nn.Module):
@@ -120,7 +119,8 @@ class PreNorm(nn.Module):
 class Block(nn.Module):
     def __init__(self, dim, dim_out, groups=8):
         super().__init__()
-        self.proj = nn.Conv3d(dim, dim_out, (1, 3, 3), padding=(0, 1, 1))
+        # lappala
+        self.proj = nn.Conv3d(dim, dim_out, (3, 3, 3), padding=(1, 1, 1))
         self.norm = nn.GroupNorm(groups, dim_out)
         self.act = nn.SiLU()
 
@@ -177,8 +177,7 @@ class SpatialLinearAttention(nn.Module):
         x = rearrange(x, 'b c f h w -> (b f) c h w')
 
         qkv = self.to_qkv(x).chunk(3, dim=1)
-        q, k, v = rearrange_many(
-            qkv, 'b (h c) x y -> b h c (x y)', h=self.heads)
+        q, k, v = rearrange_many(qkv, 'b (h c) x y -> b h c (x y)', h=self.heads)
 
         q = q.softmax(dim=-2)
         k = k.softmax(dim=-1)
@@ -187,8 +186,7 @@ class SpatialLinearAttention(nn.Module):
         context = torch.einsum('b h d n, b h e n -> b h d e', k, v)
 
         out = torch.einsum('b h d e, b h d n -> b h e n', context, q)
-        out = rearrange(out, 'b h c (x y) -> b (h c) x y',
-                        h=self.heads, x=h, y=w)
+        out = rearrange(out, 'b h c (x y) -> b (h c) x y', h=self.heads, x=h, y=w)
         out = self.to_out(out)
         return rearrange(out, '(b f) c h w -> b c f h w', b=b)
 
@@ -325,8 +323,7 @@ class Unet3D(nn.Module):
             dim, heads=attn_heads, dim_head=attn_dim_head, rotary_emb=rotary_emb))
 
         # realistically will not be able to generate that many frames of video... yet
-        self.time_rel_pos_bias = RelativePositionBias(
-            heads=attn_heads, max_distance=32)
+        self.time_rel_pos_bias = RelativePositionBias(heads=attn_heads, max_distance=32)
 
         # initial conv
 
@@ -334,8 +331,8 @@ class Unet3D(nn.Module):
         assert is_odd(init_kernel_size)
 
         init_padding = init_kernel_size // 2
-        self.init_conv = nn.Conv3d(channels, init_dim, (1, init_kernel_size,
-                                   init_kernel_size), padding=(0, init_padding, init_padding))
+        # lappala
+        self.init_conv = nn.Conv3d(channels, init_dim, (init_kernel_size, init_kernel_size, init_kernel_size), padding=(init_padding, init_padding, init_padding))
 
         self.init_temporal_attn = Residual(
             PreNorm(init_dim, temporal_attn(init_dim)))
@@ -460,18 +457,28 @@ class Unet3D(nn.Module):
                     ), 'cond must be passed in if cond_dim specified'
         batch, device = x.shape[0], x.device
 
+        print('\nstart ', x.shape)
+
         # pad to make downsizeable
         x, padding_sizes = pad_to_multiple(x, self.pad_divisors)
+
+        print('\npadding ', x.shape)
 
         focus_present_mask = default(focus_present_mask, lambda: prob_mask_like(
             (batch,), prob_focus_present, device=device))
 
+        # x.shape[2] is the depth. I don't like this
         time_rel_pos_bias = self.time_rel_pos_bias(x.shape[2], device=x.device)
 
         x = self.init_conv(x)
         r = x.clone()
 
+        print('\ninit ', x.shape)
+
+        # temporal attention is referred to the depth of the image, not to time t
         x = self.init_temporal_attn(x, pos_bias=time_rel_pos_bias)
+
+        print('\ntemp ', x.shape)
 
         t = self.time_mlp(time) if exists(self.time_mlp) else None
 
@@ -494,32 +501,40 @@ class Unet3D(nn.Module):
 
         for block1, block2, spatial_attn, temporal_attn, downsample in self.downs:
             x = block1(x, t)
+            print('\nb1 ', x.shape)
             x = block2(x, t)
+            print('\nb2 ', x.shape)
             x = spatial_attn(x)
-            x = temporal_attn(x, pos_bias=time_rel_pos_bias,
-                              focus_present_mask=focus_present_mask)
+            x = temporal_attn(x, pos_bias=time_rel_pos_bias, focus_present_mask=focus_present_mask)
+            print('\nspatial and temporal ', x.shape)
             h.append(x)
             x = downsample(x)
+            print('\ndown ', x.shape)
+            time_rel_pos_bias = self.time_rel_pos_bias(x.shape[2], device=x.device)
 
         x = self.mid_block1(x, t)
         x = self.mid_spatial_attn(x)
-        x = self.mid_temporal_attn(
-            x, pos_bias=time_rel_pos_bias, focus_present_mask=focus_present_mask)
+        x = self.mid_temporal_attn(x, pos_bias=time_rel_pos_bias, focus_present_mask=focus_present_mask)
         x = self.mid_block2(x, t)
+
+        print('\nmid ', x.shape)
 
         for block1, block2, spatial_attn, temporal_attn, upsample in self.ups:
             x = torch.cat((x, h.pop()), dim=1)
             x = block1(x, t)
             x = block2(x, t)
             x = spatial_attn(x)
-            x = temporal_attn(x, pos_bias=time_rel_pos_bias,
-                              focus_present_mask=focus_present_mask)
+            x = temporal_attn(x, pos_bias=time_rel_pos_bias, focus_present_mask=focus_present_mask)
             x = upsample(x)
+            print('\nup ', x.shape)
+            time_rel_pos_bias = self.time_rel_pos_bias(x.shape[2], device=x.device)
 
         x = torch.cat((x, r), dim=1)
         x = self.final_conv(x)
+        print('\nfinal ', x.shape)
         
         # crop to initial shape
         x = crop_to_original(x, padding_sizes)
+        print('\nend ', x.shape)
 
         return x

train/train_ddpm.py
@@ -5,6 +5,7 @@ from omegaconf import DictConfig, OmegaConf, open_dict
 import wandb
 
 import torch
+from torch.utils.data import DataLoader, DistributedSampler
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -34,12 +35,22 @@ def train(rank, world_size, cfg: DictConfig):
     # Setup distributed training
     setup(rank, world_size)
 
-    # Update results folder and update learning rate based on world size
+    results_folder = os.path.join(cfg.model.results_folder, cfg.dataset.name, cfg.model.results_folder_postfix, cfg.model.run_name)
+    print("Setting results_folder to {}".format(results_folder))
+
+    # automatically adjust learning rate following https://arxiv.org/abs/1706.02677
+    batch_rate = world_size*cfg.model.gradient_accumulate_every
+    actual_batch_size = cfg.model.batch_size*batch_rate
+    actual_lr = cfg.model.train_lr*batch_rate
+    print(f'Batch size is {cfg.model.batch_size}\tActual batch size is {actual_batch_size}')
+    print(f'Learning rate is {cfg.model.train_lr}\tActual learning rate is {actual_lr}')
+
     with open_dict(cfg):
-        cfg.model.results_folder = os.path.join(cfg.model.results_folder, cfg.dataset.name, cfg.model.results_folder_postfix, cfg.model.run_name)
-        base_lr = cfg.model.train_lr
-        cfg.model.train_lr = base_lr * world_size
-        print(f'World size is {world_size}. LR updated from {base_lr} to {cfg.model.train_lr}')
+        cfg.model.actual_batch_size = actual_batch_size
+        cfg.model.base_lr = cfg.model.train_lr
+        cfg.model.train_lr = actual_lr
+
+        cfg.model.results_folder = results_folder
 
     # Init wandb only in first process
     if rank == 0:
@@ -48,6 +59,11 @@ def train(rank, world_size, cfg: DictConfig):
         wandb.config.update(OmegaConf.to_container(cfg.model))
 
     train_dataset, val_dataset, _ = get_dataset(cfg)
+
+    # Define dataloaders
+    sampler = DistributedSampler(train_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank())
+    dl = DataLoader(train_dataset, batch_size=cfg.model.batch_size, pin_memory=True, num_workers=cfg.model.num_workers, sampler=sampler)
+    val_dl = DataLoader(val_dataset, batch_size=cfg.model.batch_size, shuffle=False, pin_memory=True, num_workers=cfg.model.num_workers)
 
     # Define conditioning parameters
     if cfg.model.cond:
@@ -58,7 +74,7 @@ def train(rank, world_size, cfg: DictConfig):
         use_class_cond = False
 
     unet3d = Unet3D(
-            dim=cfg.model.diffusion_w, # It is the channel dimension after init_conv. Why do we use w?
+            dim=cfg.model.dim, # It is the channel dimension after init_conv. Why do we use w?
             dim_mults=cfg.model.dim_mults,
             channels=cfg.model.diffusion_num_channels,
             cond_dim=cond_dim,
@@ -81,10 +97,11 @@ def train(rank, world_size, cfg: DictConfig):
 
     trainer = Trainer(
         ddp_diffusion,
-        dataset=train_dataset,
-        val_dataset=val_dataset,
+        dl=dl,
+        val_dl=val_dl,
         ema_decay=cfg.model.ema_decay,
         train_batch_size=cfg.model.batch_size,
+        base_lr=cfg.model.base_lr,
         train_lr=cfg.model.train_lr,
         train_num_steps=cfg.model.train_num_steps,
         gradient_accumulate_every=cfg.model.gradient_accumulate_every,
@@ -94,7 +111,7 @@ def train(rank, world_size, cfg: DictConfig):
         num_sample_rows=cfg.model.num_sample_rows,
         num_workers=cfg.model.num_workers,
         conditioned=cfg.model.cond,
-        rank=rank
+        rank=rank,
     )
 
     if cfg.model.load_milestone:
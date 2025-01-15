3d70edca0006632626baac518fc51323500ea5cd
Giacomo Melacini <giachimela@gmail.com>
2025-01-15 14:53:53 +0100

    Updated scripts

--- scripts/calculate_similarity.sh
+++ scripts/calculate_similarity.sh
@@ -3,11 +3,12 @@
 # Set CUDA environment variables
 export PATH="/usr/local/cuda-11.1/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11.1/lib64:$LD_LIBRARY_PATH"
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 export CPATH="/usr/local/cuda-11.1/include:$CPATH"
 
 # Activate Python virtual environment
 source .venv/bin/activate
 
 # Run Python script
-python -m memorization.calculate_similarity model=similarity dataset=allcts
+# python -m memorization.calculate_similarity model=similarity dataset=allcts
+python -m memorization.calculate_similarity +experiment=sim-baseline-gen
--- scripts/train_ddpm.sh
+++ scripts/train_ddpm.sh
@@ -3,11 +3,12 @@
 # Set CUDA environment variables
 export PATH="/usr/local/cuda-11.1/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11.1/lib64:$LD_LIBRARY_PATH"
-export CUDA_VISIBLE_DEVICES=0
 export CPATH="/usr/local/cuda-11.1/include:$CPATH"
+export CUDA_VISIBLE_DEVICES=0
 
 # Activate Python virtual environment
 source .venv/bin/activate
 
 # Run Python script
-python -m train.train_ddpm dataset=allcts model=ddpm
+# python -m train.train_ddpm dataset=allcts model=ddpm
+python -m train.train_ddpm +experiment=ddpm-t1000
